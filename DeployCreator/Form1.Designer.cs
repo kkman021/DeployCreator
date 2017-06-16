e499cb1c8a256e2735ca31f44ab922c034be05c8
楊明勳 Ming <ming@miniasp.com>
2017-06-16 12:22:12 +0800

    建立第一版

--- DeployCreator/Form1.Designer.cs
+++ DeployCreator/Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxFTPAccount = new System.Windows.Forms.TextBox();
+            this.tbxFtpUrl = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxWebDeployArg = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FTP 帳號：";
+            // 
+            // tbxFTPAccount
+            // 
+            this.tbxFTPAccount.Location = new System.Drawing.Point(82, 81);
+            this.tbxFTPAccount.Name = "tbxFTPAccount";
+            this.tbxFTPAccount.Size = new System.Drawing.Size(417, 22);
+            this.tbxFTPAccount.TabIndex = 2;
+            // 
+            // tbxFtpUrl
+            // 
+            this.tbxFtpUrl.Location = new System.Drawing.Point(82, 53);
+            this.tbxFtpUrl.Name = "tbxFtpUrl";
+            this.tbxFtpUrl.Size = new System.Drawing.Size(417, 22);
+            this.tbxFtpUrl.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "FTP 主機：";
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(82, 109);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.Size = new System.Drawing.Size(417, 22);
+            this.tbxPassword.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "FTP 密碼：";
+            // 
+            // tbxWebDeployArg
+            // 
+            this.tbxWebDeployArg.Location = new System.Drawing.Point(118, 12);
+            this.tbxWebDeployArg.Name = "tbxWebDeployArg";
+            this.tbxWebDeployArg.Size = new System.Drawing.Size(661, 22);
+            this.tbxWebDeployArg.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "WebDeploy 參數：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(791, 145);
+            this.Controls.Add(this.tbxWebDeployArg);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbxFtpUrl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxFTPAccount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Azure Publish 檔案讀取器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxFTPAccount;
+        private System.Windows.Forms.TextBox tbxFtpUrl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxWebDeployArg;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- DeployCreator/Form1.cs
+++ DeployCreator/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace DeployCreator
 {
@@ -15,6 +11,55 @@ namespace DeployCreator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = @"Text files | *.PublishSettings",
+                Multiselect = false
+            };
+            if (dialog.ShowDialog() != DialogResult.OK) return;
+
+            var path = dialog.FileName;
+
+            var fileContents = File.ReadAllText(path);
+
+            var document = XDocument.Parse(fileContents);
+
+            var deploys = document.Descendants("publishProfile")
+                .Where(x => Get(x, "publishMethod") == "MSDeploy" || Get(x, "publishMethod") == "FTP")
+                .Select(x => new
+                {
+                    DeployType = Get(x, "publishMethod"),
+                    Url = Get(x, "publishUrl"),
+                    SiteName = Get(x, "msdeploySite"),
+                    Account = Get(x, "userName"),
+                    Password = Get(x, "userPWD")
+                })
+                .ToList();
+
+            var ftp = deploys.FirstOrDefault(x => x.DeployType == "FTP");
+
+            tbxFtpUrl.Text = ftp.Url;
+            tbxFTPAccount.Text = ftp.Account;
+            tbxPassword.Text = ftp.Password;
+
+            var msDeploy = deploys.FirstOrDefault(x => x.DeployType == "MSDeploy");
+
+            tbxWebDeployArg.Text = $@"Y ""/M:https://{msDeploy.Url}/MsDeploy.axd?Site={msDeploy.SiteName.ToLower()}"" /U:{msDeploy.Account} /P:$(DeployPassword) /A:Basic ""-enableRule:DoNotDeleteRule -setParam:name='IIS Web Application Name',value='{msDeploy.SiteName.ToLower()}'""";
+
+        }
+
+        private void btnChose_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private string Get(XElement element, string name)
+        {
+            return (string) element.Attribute(name);
         }
     }
 }
